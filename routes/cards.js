a2e88766bcb166d8a718c7ce63350c3e1e3dd772
paksasha76 <pak.aleksandr2001@mail.ru>
2023-09-02 02:38:44 +0300

    project

--- routes/cards.js
+++ routes/cards.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 const {
   createCard,
   getCards,
-  delCardId,
+  deleteCard,
   likeCard,
   dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.delete('/:cardId', delCardId);
+router.delete('/:cardId', deleteCard);
 router.post('/', createCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
