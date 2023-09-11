416fc0811b8e4a39f4d64e268c13a3879ff3d5b3
paksasha76 <pak.aleksandr2001@mail.ru>
2023-09-11 16:09:14 +0300

    project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
-const { celebrate, Joi, errors } = require("celebrate");
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const { celebrate, Joi, errors } = require('celebrate');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -9,108 +9,95 @@ const limiter = rateLimit({
   legacyHeaders: false,
 });
 
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const { createUser, login } = require("./controllers/users");
+const { createUser, login } = require('./controllers/users');
 const {
   BAD_REQUEST_ERROR_CODE,
   NOT_FOUND_ERROR_CODE,
   CONFLICT_ERROR_CODE,
   INTERNAL_SERVER_ERROR_CODE,
   URL_REGULAR_EXP,
-} = require("./constants/constants");
+} = require('./constants/constants');
 
-const { userRouter } = require("./routes/users");
-const { cardRouter } = require("./routes/cards");
+const { userRouter } = require('./routes/users');
+const { cardRouter } = require('./routes/cards');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+// utils
 app.use(express.json());
 app.use(helmet());
 app.use(limiter);
 
-app.post(
-  "/signin",
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
+// authorization
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
   }),
-  login
-);
+}), login);
 
-app.post(
-  "/signup",
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(URL_REGULAR_EXP),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(URL_REGULAR_EXP),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
   }),
-  createUser
-);
+}), createUser);
 
+// protected routes with middlewares auth
 app.use(userRouter);
 app.use(cardRouter);
 
-app.use("*", (req, res) => {
-  res
-    .status(NOT_FOUND_ERROR_CODE)
-    .send({ message: "Неправильно указан запрос" });
+// wrong path
+app.use('*', (req, res) => {
+  res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Неправильно указан запрос' });
 });
 
+// celebrate error handler
 app.use(errors());
 
+// central error handler
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  if (err.name === "AuthorizationError") {
+  if (err.name === 'AuthorizationError') {
     res.status(err.statusCode).send({ message: err.message });
     return;
   }
 
-  if (err.name === "ForbiddenError") {
+  if (err.name === 'ForbiddenError') {
     res.status(err.statusCode).send({ message: err.message });
     return;
   }
 
-  if (err.name === "ValidationError") {
-    res
-      .status(BAD_REQUEST_ERROR_CODE)
-      .send({ message: "Переданы некорректные данные" });
+  if (err.name === 'ValidationError') {
+    res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные' });
     return;
   }
 
   if (err.code === 11000) {
-    res
-      .status(CONFLICT_ERROR_CODE)
-      .send({ message: "Пользователь с таким E-mail уже существует" });
+    res.status(CONFLICT_ERROR_CODE).send({ message: 'Пользователь с таким E-mail уже существует' });
     return;
   }
 
-  if (err.name === "DocumentNotFoundError") {
-    res
-      .status(NOT_FOUND_ERROR_CODE)
-      .send({ message: "Указанный _id не найден" });
+  if (err.name === 'DocumentNotFoundError') {
+    res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Указанный _id не найден' });
     return;
   }
 
-  if (err.name === "CastError") {
-    res
-      .status(BAD_REQUEST_ERROR_CODE)
-      .send({ message: "Передан некорректный _id" });
+  if (err.name === 'CastError') {
+    res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id' });
     return;
   }
 
-  res
-    .status(INTERNAL_SERVER_ERROR_CODE)
-    .send({ message: "Произошла неизвестная ошибка на сервере" });
+  res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
 });
 
 app.listen(PORT, () => {
